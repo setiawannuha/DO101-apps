4c4a5e5f5cba543966bf42cc9244520f1d27a8d9
setiawannuha <setiawannuha123@gmail.com>
2022-09-30 20:02:23 +0700

    update

--- express-helloworld/server.js
+++ express-helloworld/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send(`<h1>It Works</h1>`)
+  res.send(`<h1>It Works - Updated</h1>`)
 })
 const APP_PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080
 app.listen(APP_PORT, () => {
