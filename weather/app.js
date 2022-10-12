5a7fafd498731657eabb6a78e55f7e712e08424e
setiawannuha <setiawannuha123@gmail.com>
2022-10-12 23:57:32 +0700

    add db connection

--- weather/app.js
+++ weather/app.js
@@ -1,44 +1,58 @@
-require('dotenv').config()
-const express= require('express')
-const logger = require('morgan')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const axios = require('axios')
+require("dotenv").config();
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const db = require("./config/db");
 
-const app = express()
-app.use(cors())
-app.use(logger('combined'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.set('view engine', 'ejs');
-const OWM_API_KEY = process.env.OWM_API_KEY || 'invalid_key';
-const UNITS = process.env.UNITS || 'metric';
-app.get('/', (req, res) => {
-  res.send(`<h1>It Works</h1>`)
-})
-app.get('/weather', async(req, res) => {
+const app = express();
+app.use(cors());
+app.use(logger("combined"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+const OWM_API_KEY = process.env.OWM_API_KEY || "invalid_key";
+const UNITS = process.env.UNITS || "metric";
+app.get("/", (req, res) => {
+  res.send(`<h1>It Works</h1>`);
+});
+app.get("/weather", async (req, res) => {
   try {
-    const cityName = req.query.city || ''
-    if(cityName){
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${UNITS}&appid=${OWM_API_KEY}`
-      const { data } = await axios.get(url)
-      res.render('weather/index', {
+    const cityName = req.query.city || "";
+    if (cityName) {
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${UNITS}&appid=${OWM_API_KEY}`;
+      const { data } = await axios.get(url);
+      res.render("weather/index", {
         city: cityName,
         data,
-        symbol: UNITS === 'metric' ? '°C' : UNITS === 'imperial' ? '°F' : ''
+        symbol: UNITS === "metric" ? "°C" : UNITS === "imperial" ? "°F" : "",
       });
-    }else{
-      res.render('weather/index', {
-        city: '',
+    } else {
+      res.render("weather/index", {
+        city: "",
         data: null,
-        symbol: ''
+        symbol: "",
       });
     }
   } catch (error) {
-    res.send(`<h1>Internal server Error</h1>`)
+    res.send(`<h1>Internal server Error</h1>`);
   }
-})
-APP_PORT = process.env.PORT || 8080
+});
+app.get("/api/users", (req, res) => {
+  try {
+    db.query(`SELECT * FROM users`, (err, result) => {
+      if (err) {
+        throw Error;
+      }
+      res.json(result.rows);
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+APP_PORT = process.env.PORT || 8080;
 app.listen(APP_PORT, () => {
-  console.log(`Service running at port ${APP_PORT}`)
-})
+  console.log(`Service running at port ${APP_PORT}`);
+});
